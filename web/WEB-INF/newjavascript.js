47ffc2b60a86b3cba96495c5fd3b3e3e9f3cd56a
jonasschelde <jonasschelde@a-round-aps-air>
2016-09-04 17:39:46 +0300

    Formatting code.

--- web/WEB-INF/newjavascript.js
+++ web/WEB-INF/newjavascript.js
@@ -1,51 +1,62 @@
-window.onload = function(){
-    
-var students = [
-    {"name": "Jens", "email": "coilx14396@hotmail.com", "phone": "12345678", "Category": "red"},
-    {"name": "Thomas", "email": "x14396@hotmail.com", "phone": "12345678", "Category": "yellow"},
-    {"name": "Kim", "email": "coi96@hotmail.com", "phone": "12345678", "Category": "green"}
-    ];
-//for (var i = 0; i < students.length; i++) {
-//    console.log(students[i]);
-//}
+window.onload = function() {
 
-var tb = document.getElementById("test");
-console.log(tb);
+    var students = [{
+        "name": "Jens",
+        "email": "coilx14396@hotmail.com",
+        "phone": "12345678",
+        "Category": "red"
+    }, {
+        "name": "Thomas",
+        "email": "x14396@hotmail.com",
+        "phone": "12345678",
+        "Category": "yellow"
+    }, {
+        "name": "Kim",
+        "email": "coi96@hotmail.com",
+        "phone": "12345678",
+        "Category": "green"
+    }];
+    //for (var i = 0; i < students.length; i++) {
+    //    console.log(students[i]);
+    //}
 
-var populateTable = function(){
-    tb.innerHTML = "";
-    for (var i = 0; i < students.length; i++) {
-    var row = tb.insertRow(i);
-    row.insertCell(0).innerHTML = students [i].name;
-    row.insertCell(1).innerHTML = students [i].email;
-    row.insertCell(2).innerHTML = students [i].phone;
-    row.insertCell(3).innerHTML = students [i].Category;
+    var tb = document.getElementById("test");
+    console.log(tb);
+
+    var populateTable = function() {
+        tb.innerHTML = "";
+        for (var i = 0; i < students.length; i++) {
+            var row = tb.insertRow(i);
+            row.insertCell(0).innerHTML = students[i].name;
+            row.insertCell(1).innerHTML = students[i].email;
+            row.insertCell(2).innerHTML = students[i].phone;
+            row.insertCell(3).innerHTML = students[i].Category;
+        }
+    };
+    populateTable();
+    var studentform = document.getElementById("studentform");
+    //console.log(studentform);
+    studentform.onsubmit = function(event) {
+        event.preventdefault();
+        //console.log(studentForm.elements["full_name"].value);
+        var student = {};
+        student.name = studentform.elements[" full_name"].value;
+        student.email = studentform.elements["email"].value;
+        student.phone = studentform.elements["phone"].value;
+        student.category = studentform.elements["Category"].value;
+        //students.push(student);
+        students.push([student.name], [student.email], [student.phone], [student.category]);
+        populateTable();
+    };
+
+    // remove student
+    function deleteStudent() {
+        document.getElementById("myTable").deleteRow(0);
     }
-};
-    populateTable();
-var studentform = document.getElementById("studentform");
-//console.log(studentform);
-studentform.onsubmit = function(event){
-    event.preventdefault();
-    //console.log(studentForm.elements["full_name"].value);
-    var student = {};
-    student.name = studentform.elements[" full_name"].value;
-    student.email = studentform.elements["email"].value;
-    student.phone = studentform.elements["phone"].value;
-    student.category = studentform.elements["Category"].value;
-    //students.push(student);
-    students.push([student.name], [student.email], [student.phone], [student.category]);
-    populateTable();
-};
 
-// remove student
-function deleteStudent() {
-    document.getElementById("myTable").deleteRow(0);
-}
-
-//remove all student
-function deleteAllStudents() {
-    document.getElementById("myTable").deleteRow(0);
-}
+    //remove all student
+    function deleteAllStudents() {
+        document.getElementById("myTable").deleteRow(0);
+    }
 
 };
